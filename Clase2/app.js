60d51009c7a2180b436fa392354399517577c7f7
Agusteen <agustin.php@gmail.com>
2016-11-30 11:14:26 -0300

    Agregue Clase3

--- Clase2/app.js
+++ Clase2/app.js
@@ -3,17 +3,18 @@
 	angular.module('adivinarNumeroApp', [])
 	.controller('controlNum', controlNum);
 
-	function controlNum($scope) {
+	function controlNum($scope, $filter) {
 		$scope.verifyGuess = function () {
 			$scope.desviation = $scope.origianl - $scope.guess;
 			$scope.noOfTries = $scope.noOfTries + 1;
 		}
 
 		$scope.initializeGame = function() {
+			var round = $filter("number")($scope.guess, 0)
 			$scope.noOfTries = 0;
 			$scope.origianl = Math.floor((Math.random() * 1000) + 1);
 			$scope.guess = null;
-			$scope.desviation = null;
+			$scope.desviation = null;			
 		}
 
 		$scope.initializeGame();
