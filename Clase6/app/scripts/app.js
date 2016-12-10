f22d7c41a814732c6d10e6e84e7727d3d7c7cd15
Agusteen <agustin.php@gmail.com>
2016-12-10 12:56:55 -0300

    Clase6, uso de Yeoman

---
+++ Clase6/app/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myAppApp
+ * @description
+ * # myAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/videos', {
+        templateUrl: 'views/videos.html',       
+        
+      })
+      .when('/formulario', {
+        templateUrl: 'views/formulario.html',       
+         controller: 'FormController',
+        controllerAs: 'form'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ Clase6/app/scripts/controllers/form.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myAppApp')
+  .controller('FormController', function ($scope,$filter) {
+   
+  });
